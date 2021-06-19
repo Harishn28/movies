5d065162c7bae29a5c076518e2cbd7724e4e0905
Harish <harishn28@gmail.com>
2021-06-19 10:02:02 +0530

    Added prod optimization

--- webpackConfigs/webpack.prod.config.js
+++ webpackConfigs/webpack.prod.config.js
@@ -3,11 +3,13 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const buildDir = path.resolve(process.cwd(), 'dist');
 const appStaticAssetsPath = process.env.APP_STATIC_ASSETS_PATH;
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
     clean: true,
@@ -56,6 +58,32 @@ module.exports = {
     new webpack.DefinePlugin({
       //to make values values available in code (Ex: in file src/pages/app/Routes.js)
       'process.env':  JSON.stringify(process.env) 
-    })
+    }),
+    // new BundleAnalyzerPlugin()
   ],
+  optimization: {
+    minimize: true,
+    splitChunks: {
+      chunks: 'all',
+      minSize: 3000,
+      cacheGroups: {
+        reactVendor: {
+          test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+          name: 'react-default-vendor',
+        },
+        utilityVendor: {
+          test: /[\\/]node_modules[\\/](lodash|moment|moment-timezone)[\\/]/,
+          name: 'utility-vendor',
+        },
+        defaultVendor: {
+          test: /[\\/]node_modules[\\/](!lodash)(!moment)(!moment-timezone)[\\/]/,
+          name: 'vendor',
+        },
+        default: {
+          minChunks: 3,
+          reuseExistingChunk: true,
+        },
+      },
+    }
+  }
 };
